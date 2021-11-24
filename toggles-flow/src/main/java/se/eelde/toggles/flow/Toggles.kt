34ad0b3cba667984361f16aedaa9a6ac3ab40a19
Erik Eelde <erik.eelde@gmail.com>
2021-11-24 23:04:38 +0100

    Fix kotlin 1.6.0 issue (#118)

--- toggles-flow/src/main/java/se/eelde/toggles/flow/Toggles.kt
+++ toggles-flow/src/main/java/se/eelde/toggles/flow/Toggles.kt
@@ -1,11 +1,19 @@
 package se.eelde.toggles.flow
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 
 interface Toggles {
+    @ExperimentalCoroutinesApi
     fun toggle(key: String, defaultValue: Boolean): Flow<Boolean>
+
+    @ExperimentalCoroutinesApi
     fun toggle(key: String, defaultValue: String): Flow<String>
+
+    @ExperimentalCoroutinesApi
     fun toggle(key: String, defaultValue: Int): Flow<Int>
+
+    @ExperimentalCoroutinesApi
     fun <T : Enum<T>> toggle(
         key: String,
         type: Class<T>,
