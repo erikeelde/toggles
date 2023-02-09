28a5e2d4b8fc797cbadceec80d1ed9a7c20805f5
Erik Eelde <erik.eelde@gmail.com>
2023-02-09 23:51:31 +0100

    Restart application and delete application

--- toggles-app/src/main/java/se/eelde/toggles/configurationlist/ConfigurationViewModel.kt
+++ toggles-app/src/main/java/se/eelde/toggles/configurationlist/ConfigurationViewModel.kt
@@ -1,14 +1,22 @@
 package se.eelde.toggles.configurationlist
 
+import android.annotation.SuppressLint
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
 import android.text.TextUtils
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import se.eelde.toggles.database.WrenchApplication
 import se.eelde.toggles.database.WrenchApplicationDao
@@ -19,6 +27,7 @@ import se.eelde.toggles.database.WrenchScopeDao
 import javax.inject.Inject
 
 internal data class ViewState(
+    val application: WrenchApplication? = null,
     val configurations: List<WrenchConfigurationWithValues> = listOf(),
     val defaultScope: WrenchScope? = null,
     val selectedScope: WrenchScope? = null,
@@ -35,42 +44,49 @@ internal sealed class PartialViewState {
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
+@Suppress("StaticFieldLeak")
 class ConfigurationViewModel @Inject internal constructor(
+    @ApplicationContext private val context: Context,
     private val applicationDao: WrenchApplicationDao,
     configurationDao: WrenchConfigurationDao,
     scopeDao: WrenchScopeDao,
     val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
-    internal val applicationId: Long = savedStateHandle.get<Long>("applicationId")!!
+    private val applicationId: Long = savedStateHandle.get<Long>("applicationId")!!
 
     private val _state = MutableStateFlow(reduce(ViewState(), PartialViewState.Empty))
 
     internal val state: StateFlow<ViewState>
         get() = _state
 
-    internal lateinit var wrenchApplication: WrenchApplication
-
     private val queryString: MutableStateFlow<String> = MutableStateFlow("")
 
     init {
         setQuery(savedStateHandle.get<String>("query") ?: "")
-        viewModelScope.launch {
-            wrenchApplication = applicationDao.getApplication(applicationId)!!
-        }
 
         viewModelScope.launch {
             queryString.value = (savedStateHandle.get<String>("query") ?: "")
         }
 
         viewModelScope.launch {
-            scopeDao.getSelectedScopeFlow(applicationId = applicationId).collect { scope ->
-                _state.value = reduce(_state.value, PartialViewState.SelectedScope(scope))
-            }
-        }
-
-        viewModelScope.launch {
-            scopeDao.getDefaultScopeFlow(applicationId = applicationId).collect { scope ->
-                _state.value = reduce(_state.value, PartialViewState.DefaultScope(scope))
+            combine(
+                flowOf(applicationDao.getApplication(applicationId)!!),
+                scopeDao.getSelectedScopeFlow(applicationId = applicationId),
+                scopeDao.getDefaultScopeFlow(applicationId = applicationId),
+            ) { application, selectedScope, defaultScope ->
+                ViewState(
+                    application = application,
+                    defaultScope = defaultScope,
+                    selectedScope = selectedScope
+                )
+            }.collect { newState ->
+                _state.update {
+                    it.copy(
+                        application = newState.application,
+                        selectedScope = newState.selectedScope,
+                        defaultScope = newState.defaultScope
+                    )
+                }
             }
         }
 
@@ -108,6 +124,18 @@ class ConfigurationViewModel @Inject internal constructor(
     internal fun deleteApplication(wrenchApplication: WrenchApplication) {
         viewModelScope.launch {
             applicationDao.delete(wrenchApplication)
+        }
+    }
+
+    fun restartApplication(wrenchApplication: WrenchApplication) {
+        val activityManager =
+            context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.killBackgroundProcesses(wrenchApplication.packageName)
+
+        val intent =
+            context.packageManager.getLaunchIntentForPackage(wrenchApplication.packageName)
+        if (intent != null) {
+            context.startActivity(Intent.makeRestartActivityTask(intent.component))
         }
     }
 }
--- toggles-app/src/main/java/se/eelde/toggles/configurationlist/ConfigurationsEntry.kt
+++ toggles-app/src/main/java/se/eelde/toggles/configurationlist/ConfigurationsEntry.kt
@@ -1,20 +1,64 @@
 package se.eelde.toggles.configurationlist
 
+import android.util.Log
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Cyclone
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.FlutterDash
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import se.eelde.toggles.composetheme.AppBarState
+import se.eelde.toggles.database.WrenchApplication
 
-fun NavGraphBuilder.configurationsNavigations(navController: NavController) {
+fun NavGraphBuilder.configurationsNavigations(
+    navController: NavController,
+    onComposing: (AppBarState) -> Unit,
+) {
     composable(
         "configurations/{applicationId}",
         arguments = listOf(navArgument("applicationId") { type = NavType.LongType })
     ) {
+        val viewModel: ConfigurationViewModel = hiltViewModel()
+        val uiState = viewModel.state.collectAsState()
+
+        LaunchedEffect(key1 = uiState.value.application?.applicationLabel) {
+            onComposing(
+                AppBarState(
+                    title = "${uiState.value.application?.applicationLabel}",
+                    actions = {
+                        IconButton(onClick = {
+                            viewModel.restartApplication(uiState.value.application!!)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Outlined.Cyclone,
+                                contentDescription = null
+                            )
+                        }
+                        IconButton(onClick = {
+                            viewModel.deleteApplication(uiState.value.application!!)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Outlined.Delete,
+                                contentDescription = null
+                            )
+                        }
+                    })
+            )
+        }
+
         ConfigurationListView(
             navController = navController,
-            viewModel = hiltViewModel(),
+            uiState = uiState,
         )
     }
 }
