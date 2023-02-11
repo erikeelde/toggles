a57281c5dbefb9e5c727667442c6881e5479b13a
Erik Eelde <erik.eelde@gmail.com>
2023-02-12 00:53:31 +0100

    Some versions

--- toggles-app/src/main/java/se/eelde/toggles/dialogs/enumvalue/EnumValueView.kt
+++ toggles-app/src/main/java/se/eelde/toggles/dialogs/enumvalue/EnumValueView.kt
@@ -14,57 +14,51 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EnumValueView(
-    navController: NavController,
-    viewModel: FragmentEnumValueViewModel = hiltViewModel()
+internal fun EnumValueView(
+    state: ViewState,
+    setEnumValue: suspend (String) -> Unit,
+    revert: suspend () -> Unit,
+    popBackStack: () -> Unit,
 ) {
-
-    val uiState = viewModel.state.collectAsState()
-
     val scope = rememberCoroutineScope()
 
-    uiState.value.let {
-        Surface(modifier = Modifier.padding(16.dp)) {
-            Column {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    style = MaterialTheme.typography.headlineMedium,
-                    text = it.title ?: "TODO()"
-                )
-                LazyColumn {
-                    uiState.value.configurationValues.forEach {
-                        item {
-                            ListItem(
-                                modifier = Modifier.clickable {
-                                    scope.launch {
-                                        viewModel.saveClick(it.value.toString())
-                                        navController.popBackStack()
-                                    }
-                                },
-                                headlineText = { Text(text = it.value.toString()) }
-                            )
-                        }
+    Surface(modifier = Modifier.padding(16.dp)) {
+        Column {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                style = MaterialTheme.typography.headlineMedium,
+                text = state.title ?: ""
+            )
+            LazyColumn {
+                state.configurationValues.forEach { wrenchPredefinedConfigurationValue ->
+                    item {
+                        ListItem(
+                            modifier = Modifier.clickable {
+                                scope.launch {
+                                    setEnumValue(wrenchPredefinedConfigurationValue.value.toString())
+                                    popBackStack()
+                                }
+                            },
+                            headlineText = { Text(text = wrenchPredefinedConfigurationValue.value.toString()) }
+                        )
                     }
                 }
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-                    Button(modifier = Modifier.padding(8.dp), onClick = {
-                        scope.launch {
-                            viewModel.revertClick()
-                            navController.popBackStack()
-                        }
-                    }) {
-                        Text("Revert")
+            }
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Button(modifier = Modifier.padding(8.dp), onClick = {
+                    scope.launch {
+                        revert()
+                        popBackStack()
                     }
+                }) {
+                    Text("Revert")
                 }
             }
         }
