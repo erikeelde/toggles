115e914fe9a4f85180a30668e1097ae5a41d3a77
Erik Eelde <erik.eelde@gmail.com>
2023-08-19 01:23:38 +0200

    Make detekt happy

--- modules/applications/src/main/java/se/eelde/toggles/applications/ApplicationListView.kt
+++ modules/applications/src/main/java/se/eelde/toggles/applications/ApplicationListView.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
--- toggles-app/src/main/java/se/eelde/toggles/dialogs/stringvalue/StringValueView.kt
+++ toggles-app/src/main/java/se/eelde/toggles/dialogs/stringvalue/StringValueView.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
