27324aa036d0a81f2ac7ea829dc8d6417481181c
Erik Eelde <erik.eelde@gmail.com>
2023-08-20 01:49:11 +0200

    Bump versions in settings

--- build-logic/conventions/settings.gradle.kts
+++ build-logic/conventions/settings.gradle.kts
@@ -23,6 +23,6 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.13.4"
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+    id("com.gradle.enterprise") version "3.14.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
 }
