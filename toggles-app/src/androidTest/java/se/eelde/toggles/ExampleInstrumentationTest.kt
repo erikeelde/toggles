b8daf7a38940d4e2dbfa837b75c59634b4cc62cf
Erik Eelde <erik.eelde@gmail.com>
2023-12-03 22:43:27 +0100

    Instrumentation test scaffolding

---
+++ toggles-app/src/androidTest/java/se/eelde/toggles/ExampleInstrumentationTest.kt
@@ -0,0 +1,17 @@
+package se.eelde.toggles
+
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentationTest {
+    @Test
+    fun useAppContext() {
+        val appContext = ApplicationProvider.getApplicationContext<Application>()
+        assertEquals("se.eelde.toggles", appContext.packageName)
+    }
+}
