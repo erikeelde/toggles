8498ba1bfeddd5cc759bc78679fc76c923c37761
Erik Eelde <373219+erikeelde@users.noreply.github.com>
2026-04-26 23:02:13 +0200

    Add ScopeComparator and DefaultScopeComparator to toggles-prefs

---
+++ toggles-prefs/src/main/java/se/eelde/toggles/prefs/ScopeComparator.kt
@@ -0,0 +1,20 @@
+package se.eelde.toggles.prefs
+
+import se.eelde.toggles.core.ColumnNames
+import se.eelde.toggles.core.ToggleState
+
+@Suppress("LibraryEntitiesShouldNotBePublic")
+public fun interface ScopeComparator {
+    public fun hasOverride(state: ToggleState): Boolean
+}
+
+@Suppress("LibraryEntitiesShouldNotBePublic")
+public object DefaultScopeComparator : ScopeComparator {
+    override fun hasOverride(state: ToggleState): Boolean {
+        val defaultScope = state.scopes.firstOrNull { it.name == ColumnNames.ToggleScope.DEFAULT_SCOPE }
+        val selectedScope = state.scopes.maxByOrNull { it.timeStamp }
+        if (defaultScope == null || selectedScope == null) return false
+        if (defaultScope.id == selectedScope.id) return false
+        return state.configurationValues.any { it.scope == selectedScope.id }
+    }
+}
